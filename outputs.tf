76b23cbfbf9c4e39623293b244000b1947b8aed2
Song Bunchhoeun <s.bunchhoeun@dgc.gov.kh>
2026-01-29 15:37:46 +0700

    Add outputs for virtual network and subnets

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,15 @@ output "web_app_name" {
 output "function_app_name" {
   value = azurerm_linux_function_app.function.name
 }
+
+output "vnet_name" {
+  value = azurerm_virtual_network.vnet.name
+}
+
+output "subnet_ids" {
+  value = [
+    azurerm_subnet.subnet1.id,
+    azurerm_subnet.subnet2.id,
+    azurerm_subnet.subnet3.id
+  ]
+}

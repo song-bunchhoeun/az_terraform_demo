eafa176c3592e7836ebc02f1a55093d4afd95e01
Song Bunchhoeun <s.bunchhoeun@dgc.gov.kh>
2026-02-04 23:24:21 +0700

    add resources

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -10,10 +10,10 @@ output "virtual_network_id" {
   value = azurerm_virtual_network.vnet.id
 }
 
-# output "subnet_ids" {
-#   value = {
-#     subnet1 = azurerm_subnet.subnet1.id
-#     subnet2 = azurerm_subnet.subnet2.id
-#     subnet3 = azurerm_subnet.subnet3.id
-#   }
-# }
+output "subnet_ids" {
+  value = {
+    subnet1 = azurerm_subnet.subnet1.id
+    subnet2 = azurerm_subnet.subnet2.id
+    subnet3 = azurerm_subnet.subnet3.id
+  }
+}

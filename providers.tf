bb4b6bd9c1b255868cfd35d7352d555e69915f6e
Song Bunchhoeun <s.bunchhoeun@dgc.gov.kh>
2026-02-04 08:54:09 +0700

    Perform create rescources on azure with terraform

---
+++ providers.tf
@@ -0,0 +1,23 @@
+# Configure the Microsoft Azure Provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.58.0" 
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "b07e332b-f356-47ef-822e-92ff45891cc5"
+  tenant_id = "7238cf22-2f07-4cf0-a57d-85fc41978373"
+}
+
+resource "random_string" "sa_suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
+

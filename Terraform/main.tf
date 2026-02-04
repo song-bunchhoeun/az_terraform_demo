4d2e9c105ee126d3902b7e1331ce6be9a9f033bc
Song Bunchhoeun <s.bunchhoeun@dgc.gov.kh>
2026-02-04 22:01:51 +0700

    add resources

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,27 +12,27 @@ resource "azurerm_virtual_network" "vnet" {
     resource_group_name = azurerm_resource_group.rg.name
 }
 
-# # Subnet1
-# resource "azurerm_subnet" "subnet1" {
-#   name                 = "subnet1"
-#   resource_group_name  = azurerm_resource_group.rg.name
-#   virtual_network_name = azurerm_virtual_network.vnet.name
-#   address_prefixes     = ["10.0.1.0/24"]
-# }
+# Subnet1
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
 
-# # Subnet2
-# resource "azurerm_subnet" "subnet2" {
-#   name                 = "subnet2"
-#   resource_group_name  = azurerm_resource_group.rg.name
-#   virtual_network_name = azurerm_virtual_network.vnet.name
-#   address_prefixes     = ["10.0.2.0/24"]
-# }
+# Subnet2
+resource "azurerm_subnet" "subnet2" {
+  name                 = "subnet2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
 
-# # Subnet3
-# resource "azurerm_subnet" "subnet3" {
-#   name                 = "subnet3"
-#   resource_group_name  = azurerm_resource_group.rg.name
-#   virtual_network_name = azurerm_virtual_network.vnet.name
-#   address_prefixes     = ["10.0.3.0/24"]
-# }
+# Subnet3
+resource "azurerm_subnet" "subnet3" {
+  name                 = "subnet3"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
 

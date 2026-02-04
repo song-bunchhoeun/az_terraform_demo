a81e4ed79ca141210822bd30760f1f603883471d
Song Bunchhoeun <s.bunchhoeun@dgc.gov.kh>
2026-02-04 19:55:59 +0700

    add resource

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -9,3 +9,11 @@ output "resource_group_location" {
 output "virtual_network_id" {
   value = azurerm_virtual_network.vnet.id
 }
+
+output "subnet_ids" {
+  value = {
+    subnet1 = azurerm_subnet.subnet1.id
+    subnet2 = azurerm_subnet.subnet2.id
+    subnet3 = azurerm_subnet.subnet3.id
+  }
+}

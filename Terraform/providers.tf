b84e9d99783bb04e806fd00a25e8867522d48536
Song Bunchhoeun <s.bunchhoeun@dgc.gov.kh>
2026-02-05 16:45:31 +0700

    tfstatefile

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -1,18 +1,11 @@
-# # Configure the Microsoft Azure Provider
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "4.58.0" 
-#     }
-#   }
-# }
-
-provider "azurerm" {
-  features {}
-}
-
+# Configure the Microsoft Azure Provider
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.58.0" 
+    }
+  }
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
     storage_account_name = "tfstateuat9a"
@@ -21,8 +14,17 @@ terraform {
   }
 }
 
-# resource "random_string" "sa_suffix" {
-#   length  = 6
-#   upper   = false
-#   special = false
-# }
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+resource "random_string" "sa_suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}

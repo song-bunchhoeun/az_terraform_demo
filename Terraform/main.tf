bbf4bd56b0a8dfbebb82edff2325b87a870e9c10
Song Bunchhoeun <s.bunchhoeun@dgc.gov.kh>
2026-02-05 11:31:09 +0700

    test cicd

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,22 +1,22 @@
-# Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-test-1"
-  location = "Southeast Asia"
-}
+# # Resource Group
+# resource "azurerm_resource_group" "rg" {
+#   name     = "rg-test-1"
+#   location = "Southeast Asia"
+# }
 
-resource "azurerm_storage_account" "state_sa" {
-  name                     = "tfstateuat9a"
-  resource_group_name      = azurerm_resource_group.state_rg.name
-  location                 = azurerm_resource_group.state_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+# resource "azurerm_storage_account" "state_sa" {
+#   name                     = "tfstateuat9a"
+#   resource_group_name      = azurerm_resource_group.state_rg.name
+#   location                 = azurerm_resource_group.state_rg.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
 
-resource "azurerm_storage_container" "state_container" {
-  name                  = "tfstateuat8as4"
-  storage_account_id    = azurerm_storage_account.state_sa.id
-  container_access_type = "private"
-}
+# resource "azurerm_storage_container" "state_container" {
+#   name                  = "tfstateuat8as4"
+#   storage_account_id    = azurerm_storage_account.state_sa.id
+#   container_access_type = "private"
+# }
 
 # Virtual Network
 resource "azurerm_virtual_network" "vnet" {

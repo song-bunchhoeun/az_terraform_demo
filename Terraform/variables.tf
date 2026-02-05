5bbb0d4e6f5a1f11acd5b652add43063e48980a8
Song Bunchhoeun <s.bunchhoeun@dgc.gov.kh>
2026-02-05 15:33:25 +0700

    statefile

--- Terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "prefix" {
-  type        = string
-  description = "The prefix used for all resources"
-}
-
-variable "location" {
-  type        = string
-  description = "The Azure region where resources will be created"
-}

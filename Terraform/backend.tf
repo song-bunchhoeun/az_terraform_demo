8085c2e1223e444baccb7d00b6837b4c25813394
Song Bunchhoeun <s.bunchhoeun@dgc.gov.kh>
2026-02-05 10:38:24 +0700

    test cicd

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,8 +1,14 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.58.0" 
+    }
+  }
   backend "azurerm" {
-    resource_group_name  = "rg-test-1"
+    resource_group_name  = "rg-terraform-state"
     storage_account_name = "tfstateuniqueaccountname"
     container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    key                  = "prod.terraform.tfstate"
   }
 }
--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -6,12 +6,12 @@ terraform {
       version = "4.58.0" 
     }
   }
-  # backend "azurerm" {
-  #   resource_group_name  = "rg-test-1"
-  #   storage_account_name = "tfstateuniqueaccountname"
-  #   container_name       = "tfstate"
-  #   key                  = "prod.terraform.tfstate"
-  # }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "tfstateuniqueaccountname"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {

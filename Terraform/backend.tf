7039bf64aea2a15c82915a3ad88e7b074de4ef3d
Song Bunchhoeun <s.bunchhoeun@dgc.gov.kh>
2026-02-05 08:24:57 +0700

    cicd

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "TerraformStateRG"
+    resource_group_name  = "rg-test-1"
     storage_account_name = "tfstateuniqueaccountname"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"

fc26c28c980f6cd75da66fde4775012b43011ca1
Song Bunchhoeun <s.bunchhoeun@dgc.gov.kh>
2026-02-05 09:28:50 +0700

    ci_cd

--- terraform-bootstrap/main.tf
+++ terraform-bootstrap/main.tf
@@ -9,8 +9,8 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_storage_account" "tfstate" {
   name                     = "tfstateuniqueacct01"
-  resource_group_name      = azurerm_resource_group.rg
-  location                 = azurerm_resource_group.rg
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }

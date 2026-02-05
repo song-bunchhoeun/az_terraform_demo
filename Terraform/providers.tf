fd137e2f288a18cd025021b4961be2a3c2cbd450
Song Bunchhoeun <s.bunchhoeun@dgc.gov.kh>
2026-02-05 13:58:51 +0700

    backend

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -6,12 +6,12 @@ terraform {
       version = "4.58.0" 
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "rg-test-1"
-    storage_account_name = "tfstateuat9a"
-    container_name       = "tfstateuat8as4"
-    key                  = "prod.terraform.tfstate"
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "rg-test-1"
+  #   storage_account_name = "tfstateuat9a"
+  #   container_name       = "tfstateuat8as4"
+  #   key                  = "prod.terraform.tfstate"
+  # }
 }
 
 provider "azurerm" {

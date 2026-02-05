82fcce3f94d579cf34bbd256a90f1de9c51bf399
Song Bunchhoeun <s.bunchhoeun@dgc.gov.kh>
2026-02-05 14:45:08 +0700

    New Message: Thu Feb  5 14:45:08 +07 2026

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -2,9 +2,9 @@
 #   value = azurerm_resource_group.rg.name
 # }
 
-output "resource_group_location" {
-  value = azurerm_resource_group.rg.location
-}
+# output "resource_group_location" {
+#   value = azurerm_resource_group.rg.location
+# }
 
 output "virtual_network_id" {
   value = azurerm_virtual_network.vnet.id

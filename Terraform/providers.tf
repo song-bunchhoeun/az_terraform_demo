fb51db519aee3f3c126b499ad5e60ad316ac6e53
Song Bunchhoeun <s.bunchhoeun@dgc.gov.kh>
2026-02-05 14:31:44 +0700

    backend

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -6,12 +6,6 @@ terraform {
       version = "4.58.0" 
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "rg-test-1"
-    storage_account_name = "tfstateuat9a"
-    container_name       = "tfstateuat8as4"
-    key                  = "prod.terraform.tfstate"
-  }
 }
 
 provider "azurerm" {
--- gitpush.sh
+++ gitpush.sh
@@ -1,4 +1,10 @@
-!#/bin/bash
+#!/bin/bash
 git add .
-git commit -m "message"
+echo "Enter your commit message:"
+read -r commit_message
+if [ -z "$commit_message" ]; then
+  commit_message="New Message: $(date)"
+  echo "No message entered, using default."
+fi
+git commit -m "$commit_message"
 git push

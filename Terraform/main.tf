619ed88eb6fea140669e4ee00bea31c4725a48e0
Song Bunchhoeun <s.bunchhoeun@dgc.gov.kh>
2026-02-06 10:05:06 +0700

    Add: waf policy

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -36,10 +36,17 @@ resource "azurerm_subnet" "subnet3" {
   address_prefixes     = ["10.0.3.0/24"]
 }
 
+# Front Door
 resource "azurerm_network_security_group" "nsg_frontdoor" {
-  # This creates: "ekyc-nonprod-nsg-frontdoor"
   name                = "${var.prefix}-nsg-frontdoor"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   tags                = var.tags
+}
+
+# WAF Policy 
+resource "azurerm_network_security_group" "nsg_wafpolicy" {
+  name                = "${var.prefix}-waf-uat-1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }

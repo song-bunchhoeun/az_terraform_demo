036e33e14268fabb4fd316ac0f16c91ac55b4a86
Song Bunchhoeun <s.bunchhoeun@dgc.gov.kh>
2026-02-06 09:32:26 +0700

    Add: resources

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -40,6 +40,6 @@ resource "azurerm_network_security_group" "nsg_frontdoor" {
   # This creates: "ekyc-nonprod-nsg-frontdoor"
   name                = "${var.prefix}-nsg-frontdoor"
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
   tags                = var.tags
 }

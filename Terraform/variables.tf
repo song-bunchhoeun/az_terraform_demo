79fc47ccc9b3f75e4da51966eb167cfcf0c14e20
Song Bunchhoeun <s.bunchhoeun@dgc.gov.kh>
2026-02-06 09:12:18 +0700

    add resources

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -11,3 +11,25 @@ variable "vnet_address_space" {
   type    = list(string)
   default = ["10.0.0.0/16"]
 }
+
+variable "subnets" {
+  description = "A map of subnets to create"
+  type = map(object({
+    name             = string
+    address_prefixes = list(string)
+  }))
+  default = {
+    "subnet1" = {
+      name             = "subnet1"
+      address_prefixes = ["10.0.1.0/24"]
+    },
+    "subnet2" = {
+      name             = "subnet2"
+      address_prefixes = ["10.0.2.0/24"]
+    },
+    "subnet3" = {
+      name             = "subnet3"
+      address_prefixes = ["10.0.3.0/24"]
+    }
+  }
+}

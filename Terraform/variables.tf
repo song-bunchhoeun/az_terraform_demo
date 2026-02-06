a1dcfe577dea434f2a40a142480810523d622e3b
Song Bunchhoeun <s.bunchhoeun@dgc.gov.kh>
2026-02-06 09:31:05 +0700

    add: resources

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -34,21 +34,6 @@ variable "subnets" {
   }
 }
 
-variable "prefix" {
-  description = "A prefix for all resource names"
-  type        = string
-}
-
-variable "location" {
-  description = "The Azure region for deployment"
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  type        = string
-}
-
 variable "tags" {
   description = "A mapping of tags to assign to the resource"
   type        = map(string)
